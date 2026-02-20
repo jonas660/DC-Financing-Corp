489b2733765f0f9cf9e6b50e5b371f5a564d18da
Jonas <sariegojonas16@gmail.com>
2026-02-20 23:43:30 +0800

    Update apps

--- motofinai/apps/pos/create_superuser_view.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
-def create_superuser(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'yourpassword')
-        return HttpResponse("Superuser created!")
-    return HttpResponse("Already exists.")
--- theme/apps.py
+++ theme/apps.py
@@ -3,3 +3,20 @@ from django.apps import AppConfig
 
 class ThemeConfig(AppConfig):
     name = 'theme'
+
+from django.apps import AppConfig
+
+class YourAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yourapp'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="admin123"
+            )
